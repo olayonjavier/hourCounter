dc2843b928015317d2e8f195087080cbf60d2ff7
Javi <olayon.javier@gmail.com>
2019-03-09 01:01:11 -0500

    set up calculations for finding missing work hours for the week

--- src/hourcounter/HourCounter.java
+++ src/hourcounter/HourCounter.java
@@ -6,11 +6,10 @@ import java.util.Scanner;
 public class HourCounter {
 
 	static Scanner input = new Scanner(System.in);
-	
 	static final int MIN_IN_HOUR = 60;
+	static final int MAX_HOURS = 20;
 	
 	public static void main(String[] args) {
-		
 		
 		System.out.println("Enter how many days worked: ");
 		int days = input.nextInt();
@@ -23,14 +22,13 @@ public class HourCounter {
 			minutes += getMinutes();
 		}	
 		
-		totalHours = convertMinutesToHours(minutes);
 		remainderMinutes = getRemainderMinutes(minutes);
 		
-		System.out.println("Total Time Worked = " + totalHours + " hours and " + remainderMinutes + " minutes");
+		System.out.println("Total Time Worked = " + convertMinutesToHours(minutes) + " hours and " + remainderMinutes + " minutes");
+		System.out.println("Remaining Hours = " + getMissingHours(minutes) + " hours and " + getMissingMinutes(remainderMinutes) + " minutes");
 	}
 	
 	public static int getMinutes(){
-
 		return input.nextInt();
 	}
 	
@@ -40,6 +38,16 @@ public class HourCounter {
 	
 	public static int getRemainderMinutes(int totalMinutes){
 		return totalMinutes % MIN_IN_HOUR;
+	}
+	
+	public static int getMissingHours(int minutes){
+		int missingMinutes = (MAX_HOURS * MIN_IN_HOUR) - minutes;
+		
+		return convertMinutesToHours(missingMinutes);
+	}
+	
+	public static int getMissingMinutes(int remainderMinutes){
+		return MIN_IN_HOUR - remainderMinutes;
 	}
 	
 	
