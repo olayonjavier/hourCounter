812ae7882a5ed00ca691bbc8e8903f023dbafd1c
Javi <olayon.javier@gmail.com>
2019-03-08 23:14:42 -0500

    set up initial input, and logic for calculations

--- src/hourcounter/HourCounter.java
+++ src/hourcounter/HourCounter.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hourcounter;
 
-/**
- *
- * @author Javi
- */
+import java.util.Scanner;
+
 public class HourCounter {
 
-	/**
-	 * @param args the command line arguments
-	 */
+	
 	public static void main(String[] args) {
-		// TODO code application logic here
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter how many days worked: ");
+		int days = input.nextInt();
+		int minutes = 0;
+		int totalHours = 0;
+		int remainderMinutes = 0;
+		
+		for(int currentDay = 1; currentDay <= days; currentDay++){
+			System.out.println("Enter minutes for day " + currentDay);
+			minutes += input.nextInt();
+		}	
+		
+		totalHours = minutes / 60;
+		remainderMinutes = minutes % 60;
+		
+		System.out.println("Total Hours = " + totalHours + " " + remainderMinutes + " minutes");
 	}
 	
 }
